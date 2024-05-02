2a42ff3ee49f418aa75b94e50044d36e8657fb44
Дмитрий Леонов <dmitriy.leonov.1993@mail.ru>
2024-05-03 01:57:42 +0500

    Initial commit

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from catalog.apps import CatalogConfig
+from catalog.views import home_page, contact_info
+
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home_page, name='home_page'),
+    path('contacts/', contact_info, name='contact_info')
+]
