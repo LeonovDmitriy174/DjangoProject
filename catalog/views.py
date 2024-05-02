665a2a1c107c28f299d7afc0b4214af93ba63ba9
Дмитрий Леонов <dmitriy.leonov.1993@mail.ru>
2024-05-03 02:08:00 +0500

    Добавил обработку контактной информации

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,8 @@ def home_page(request):
 
 
 def contact_info(request):
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+    message = request.POST.get('message')
+    print(name, phone, message)
     return render(request, 'contact_info.html')
